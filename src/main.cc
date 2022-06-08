1f410fe660115f08eeb8ed2411a650b254cfcc02
Ancelin Bouchet <ancelin.bouchet@epita.fr>
2022-06-08 15:53:25 +0200

    WIP feat: implemented OpenGL main function

--- src/main.cc
+++ src/main.cc
@@ -1,34 +1,86 @@
+#include <GL/freeglut.h>
+#include <GL/gl.h>
+#include <GL/glew.h>
 #include <iostream>
 
 #include "matrix4/matrix4.hh"
 #include "utils/utils.hh"
 
-int main(/* int argc, char *argv[] */)
+inline bool checkGLError(const char *const file, int line)
+{
+    GLenum error_code = glGetError();
+    if (error_code != GL_NO_ERROR)
+    {
+        std::cerr << "\e[31m[OpenGL Error]\e[39m " << file << " at line "
+                  << line << ": got code `" << ((int)error_code) << "`"
+                  << std::endl;
+    }
+}
+
+#define CHECK_GL_ERROR() checkGLError(__FILE__, __LINE__)
+
+bool init_glut(int &argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitContextVersion(4, 5);
+    glutInitContextProfile(GLUT_CORE_PROFILE);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowSize(1024, 1024);
+    glutInitWindowPosition(10, 10);
+    glutCreateWindow("Test OpenGL - POGL");
+    return true;
+}
+
+bool init_glew()
+{
+    return (glewInit() == GLEW_OK);
+}
+
+bool init_GL()
+{
+    constexpr int PIXEL_BYTE_ALIGNEMENT_LEN = 1; // 1 byte for 8-bit color
+    glEnable(GL_DEPTH_TEST);
+    CHECK_GL_ERROR();
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    CHECK_GL_ERROR();
+    glEnable(GL_CULL_FACE);
+    CHECK_GL_ERROR();
+    glClearColor(0.5, 0.5, 0.5, 1.0);
+    CHECK_GL_ERROR();
+    glPixelStorei(GL_PACK_ALIGNMENT, PIXEL_BYTE_ALIGNEMENT_LEN);
+    CHECK_GL_ERROR();
+    glPixelStorei(GL_UNPACK_ALIGNMENT, PIXEL_BYTE_ALIGNEMENT_LEN);
+    CHECK_GL_ERROR();
+
+    return true;
+}
+
+bool init_shaders()
+{
+    return true;
+}
+
+bool init_object()
+{
+    return true;
+}
+
+bool init_POV()
+{
+    return true;
+}
+
+int main(int argc, char *argv[])
 {
     using namespace pogl;
-    const auto A = Matrix4(Matrix4::ElementsBufferType{
-        1, 0, 0, 0, // l1
-        2, 0, 0, 0, // l2
-        3, 0, 0, 0, // l3
-        4, 0, 0, 0 // l4
-    });
-    std::cout << "A:\n" << A << "\n";
 
-    const auto B = Matrix4(Matrix4::ElementsBufferType{
-        1, 2, 3, 4, // l1
-        0, 0, 0, 0, // l2
-        0, 0, 0, 0, // l3
-        0, 0, 0, 0 // l4
-    });
-    std::cout << "B:\n" << B << "\n";
+    init_glut(argc, argv);
+    init_glew();
+    init_GL();
+    init_shaders();
+    init_object();
+    init_POV();
+    glutMainLoop();
 
-    const auto matmul_res = A * B;
-    std::cout << "res:\n" << matmul_res << "\n";
-
-    const auto frustum = Matrix4::frustum(-5, 5, -5, 5, 1, 100);
-    std::cout << "frustum:\n" << frustum << "\n";
-
-    const auto look_at = Matrix4::look_at(5, 6, 7, 13, 14, 15, 1, 0, 0);
-    std::cout << utils::pretty_print_on << "look_at:\n" << look_at <<"\n";
     return 0;
 }
