80df51bc0500341d937d2e0148bbf978d782f60c
Ancelin Bouchet <ancelin.bouchet@epita.fr>
2022-06-15 16:36:11 +0200

    feat: Updated shaders with uniforms

--- resources/shaders/uniform/fragment.glsl
+++ resources/shaders/uniform/fragment.glsl
@@ -2,6 +2,8 @@
 
 layout(location=0) out vec4 output_color;
 
+uniform vec4 obj_color;
+
 void main() {
-    output_color = vec4(1.0,0.0,0.0,1.0);
+    output_color = obj_color;
 }
--- resources/shaders/uniform/vertex.glsl
+++ resources/shaders/uniform/vertex.glsl
@@ -2,6 +2,9 @@
 
 in vec3 position;
 
+uniform mat4 projection_matrix;
+uniform mat4 model_view_matrix;
+
 void main() {
-    gl_Position = vec4(position,1.0);
+    gl_Position = projection_matrix * model_view_matrix * vec4(position,1.0);
 }
