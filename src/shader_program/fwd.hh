d75d47a7c88e0e217ce8ab26f9e2d4c7a0fb2cc9
Ancelin Bouchet <ancelin.bouchet@epita.fr>
2022-07-02 13:39:34 +0200

    refactor: Split ShaderProgram and Uniform in separate files

---
+++ src/shader_program/fwd.hh
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace pogl
+{
+    class ShaderProgram;
+    class Uniform;
+} // namespace pogl
--- src/shader_program/shader_program.hh
+++ src/shader_program/shader_program.hh
@@ -6,7 +6,9 @@
 #include <optional>
 #include <string>
 
+#include "fwd.hh"
 #include "matrix4/matrix4.hh"
+#include "uniform.hh"
 #include "vector4/vector4.hh"
 
 namespace pogl
@@ -19,33 +21,7 @@ namespace pogl
         using Self = ShaderProgram;
         using ShaderIdType = GLuint;
         using ProgramIdType = GLuint;
-
-        class Uniform
-        {
-        public:
-            using LocType = GLuint;
-            using SizeType = GLint;
-            using TypeEnum = GLenum;
-            Uniform();
-            Uniform(const std::string &name, LocType location, TypeEnum type,
-                    SizeType size, ShaderProgram *program);
-
-            void set_mat4(const Matrix4 &mat);
-            void set_float(GLfloat value);
-            void set_vec4(const Vector4 &vect);
-            void set_vec4(GLfloat x, GLfloat y, GLfloat z, GLfloat w);
-            inline TypeEnum type() const
-            {
-                return _type;
-            };
-
-        private:
-            LocType _location;
-            SizeType _size;
-            TypeEnum _type;
-            std::string _name;
-            ShaderProgram *_program;
-        };
+        using Uniform = pogl::Uniform;
 
         using UniformMapType = std::map<std::string, Uniform>;
 
