767b4784bbe07bed1bb534567cb320b1694f4908
Ancelin Bouchet <ancelin.bouchet@epita.fr>
2022-07-02 18:19:12 +0200

    feat: Made textured shader discard transparent pixels

--- resources/shaders/textured/fragment.glsl
+++ resources/shaders/textured/fragment.glsl
@@ -7,5 +7,8 @@ layout(location=0) out vec4 color;
 uniform sampler2D uTexture;
 
 void main() {
-    color = texture(uTexture, uv);
+    vec4 tex_color = texture(uTexture, uv);
+    if(tex_color.a < 0.1)
+        discard;
+    color = tex_color;
 }
