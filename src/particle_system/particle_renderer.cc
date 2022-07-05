aa58886717b5780f1abc272835addfd63038e118
Ancelin Bouchet <ancelin.bouchet@epita.fr>
2022-07-05 23:47:32 +0200

    misc: Changed particle scale 0.225

--- src/particle_system/particle_renderer.cc
+++ src/particle_system/particle_renderer.cc
@@ -48,7 +48,7 @@ namespace pogl {
         const auto x_axis = Vector3::up().cross(y_axis).normalized();
         const auto z_axis = y_axis.cross(x_axis).normalized();
         const auto rotation_transform = Matrix4::basis_change(x_axis, y_axis, z_axis)
-            * Matrix4::scale(0.3);
+            * Matrix4::scale(0.225);
 
         for(size_t i = 0; i < particles->size(); i++) {
             const auto &p = particles->at(i);
