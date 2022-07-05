152985ec41541b1eb00ad84380b564f1a7d2cc87
Ancelin Bouchet <ancelin.bouchet@epita.fr>
2022-07-05 23:48:18 +0200

    cleanup: Removed dead code

--- src/particle_system/particle_renderer.cc
+++ src/particle_system/particle_renderer.cc
@@ -55,7 +55,6 @@ namespace pogl {
             auto center = p.getPosition();
             for(size_t j = 0; j < 4; j++) {
                 const auto vert_pos = (rotation_transform * Matrix4::rotate(p.getRotation(), "y") * Vector4(VERTICES[j * 2], 0, VERTICES[j * 2 + 1], 1)).to_spatial() + center;
-                // const auto vert_pos = (rotation_transform * Vector4(VERTICES[j * 2], 0, VERTICES[j * 2 + 1], 1)).to_spatial() + center;
                 vertexPositionData[i*12+j*3] = vert_pos.x;
                 vertexPositionData[i*12+j*3+1] = vert_pos.y;
                 vertexPositionData[i*12+j*3+2] = vert_pos.z;
